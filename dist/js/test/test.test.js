42040b308c234f064e0e55864dd9f5d1b86e3191
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2018-10-04 10:41:17 -0400

    Added testing infrastructure

---
+++ dist/js/test/test.test.js
@@ -0,0 +1,8 @@
+"use strict";
+
+describe("Testing", function () {
+  it("is fun", function () {// ?
+  });
+  it("no really", function () {// ?
+  });
+});
---
+++ src/js/test/test.test.js
@@ -0,0 +1,8 @@
+describe("Testing", function() {
+  it("is fun", function() {
+    // ?
+  });
+  it("no really", function() {
+    // ?
+  });
+});
